57006ad8c7559cc22f7911789add498a80eed56b
verumlotus <universumtremendum@gmail.com>
2022-02-28 15:51:29 -0800

    Add comment blocks

--- src/Contract.sol
+++ src/Contract.sol
@@ -1,4 +1,21 @@
 // SPDX-License-Identifier: Unlicense
 pragma solidity ^0.8.11;
 
-contract Contract {}
+/**
+ * @title title
+ * @notice description
+ * @author verum 
+ */
+contract Contract {
+    /************************************************
+     *  STORAGE
+    ***********************************************/
+
+    /************************************************
+     *  IMMUTABLES & CONSTANTS
+    ***********************************************/
+
+    /************************************************
+     *  EVENTS, ERRORS, MODIFIERS
+    ***********************************************/
+}
